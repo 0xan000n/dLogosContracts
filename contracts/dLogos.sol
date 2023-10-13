eb6b38feba7907674deb507fcdf770722aa51361
0xan000n <0xan000n@protonmail.com>
2023-10-12 18:42:45 -0700

    add 3 cases for refund

--- contracts/dLogos.sol
+++ contracts/dLogos.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./interfaces/IdLogos.sol";
 
 /* OPEN QUESTIONS 
     1. Does there need to be a dedicated contribution address for each conversation?
@@ -15,98 +16,25 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /// @title Core dLogos contract
 /// @author 0xan000n
-contract dLogos is ReentrancyGuard {
-    struct Backer {
-        address addr;
-        uint256 amount; // ETH Contribution
-        bool isDistributed; // Funds have been distributed
-    }
+contract dLogos is IdLogos, ReentrancyGuard {
+    uint256 public logoID = 1; // Global Logo ID starting from 1
+    mapping(uint256 => Logo) public logos; // Mapping of Owner addresses to Logo ID to Logo info
+    mapping(uint256 => Backer[]) public logoBackers;  // Mapping of Logo ID to list of Backers
+    mapping(uint256 => Speaker[]) public logoSpeakers; // Mapping of Logo ID to list of Speakers
+    mapping(uint256 => Host[]) public logoHosts; // Mapping of Logo ID to list of Hosts
+    uint16 public dLogosServiceFee = 300; // dLogos fees in BPS (3%)
 
-    enum SpeakerStatus {
-        Pending,
-        Accepted,
-        Declined
-    }
-
-    struct Speaker {
-        address addr;
-        uint16 fee; // Speaker reward BPS
-        string provider; // e.g. X, Discord etc.
-        string handle;
-        SpeakerStatus status; // Status of the speaker 0 = pending, 1 = accepted, 2 = declined
-    }
-
-    struct Host {
-        address addr;
-        uint16 fee; // Host reward BPS
-    }
-
-    /// @notice All on-chain information for a Logo.
-    struct Logo {
-        // Meta
-        uint256 id;
-        string title;
-        string description;
-        string discussion;
-        // Roles
-        address creator;
-        uint scheduledAt;
-        // Crowdfunding Attributes
-        uint crowdfundStartAt;
-        uint crowdfundEndAt;
-        bool isCrowdfunding;
-        /*
-        address[] speakers;
-        address[] backers;      // BackerInfo[] backers;
-    
-        
-
-        // Scheduling Attributes
-        uint scheduledAt;
-        uint scheduleFailedRefundAt; // Date to allow refund if conversation is not scheduled.
-        bool isScheduled;
-        
-        // Media Upload Attributes
-        uint uploadFailureRefundAt; // Date to allow refund if conversation is not uploaded.
-        
-        */
-        string mediaAssetURL;
-        bool isUploaded;
-        // Logo Split Address
-        address splits;
-    }
-
-    // Global Logo ID starting from 1
-    uint256 public logoID = 1;
-    // Mapping of Owner addresses to Logo ID to Logo info
-    mapping(uint256 => Logo) public logos;
-    // Mapping of Logo ID to list of Backers
-    mapping(uint256 => Backer[]) public logoBackers;
-    // Mapping of Logo ID to list of Speakers
-    mapping(uint256 => Speaker[]) public logoSpeakers;
-    // Mapping of Logo ID to list of Hosts
-    mapping(uint256 => Host[]) public logoHosts;
-    // dLogos fees in BPS (3%)
-    uint16 public dLogosServiceFee = 300;
-
-    /* Events */
-
-    // Default
     event LogUpdateFee(uint16 indexed _fee);
     event LogoCreated(
         address indexed _owner,
         uint indexed _logoID,
         uint indexed _crowdfundStartAt
     );
-
-    // Crowdfund
     event Crowdfund(address indexed _owner, uint indexed _amount);
     event CrowdfundToggle(
         address indexed _owner,
         bool indexed _crowdfundIsOpen
     );
-
-    // Funds
     event FundsWithdrawn(address indexed _owner, uint indexed _amount);
     event SpeakersSet(
         address indexed _owner,
@@ -122,9 +50,18 @@ contract dLogos is ReentrancyGuard {
         address indexed _splitsAddress,
         uint256 indexed _totalRewards
     );
-    event SpeakerStatusSet(address indexed _speaker, uint indexed _status);
+    event SpeakerStatusSet(uint indexed _logoID, address indexed _speaker, uint indexed _status);
+    event RefundIssued(
+        uint indexed _logoID, 
+        address indexed _owner, 
+        uint indexed _amount,
+        bool _case1,
+        bool _case2,
+        bool _case3
+    );
 
-    function setServiceFee(uint16 _dLogosServiceFee) external {
+
+    function setServiceFee(uint16 _dLogosServiceFee) external nonReentrant {
         /* TODO: (1) onlyOwner */
         require(
             _dLogosServiceFee > 0 && _dLogosServiceFee <= 10000,
@@ -141,7 +78,7 @@ contract dLogos is ReentrancyGuard {
         string calldata _discussion,
         string calldata _mediaAssetURL,
         uint _crowdfundNumberOfDays
-    ) external returns (uint256) {
+    ) external nonReentrant returns (uint256) {
         /* TODO: (1) Requires (2) right role */
         logos[logoID] = Logo({
             id: logoID,
@@ -150,7 +87,7 @@ contract dLogos is ReentrancyGuard {
             discussion: _discussion,
             creator: msg.sender,
             scheduledAt: 0, // TODO: Correct default
-            mediaAssetURL: _mediaAssetURL,
+            mediaAssetURL: _mediaAssetURL, // TODO: remove this from calldata
             isUploaded: false,
             isCrowdfunding: true,
             crowdfundStartAt: block.timestamp,
@@ -168,14 +105,12 @@ contract dLogos is ReentrancyGuard {
      * @dev Open crowdfund for Logo. Only the owner of the Logo is allowed to open a crowdfund.
      * returns if successful.
      */
-    function toggleCrowdfund(uint256 _logoID) external returns (bool) {
+    function toggleCrowdfund(uint256 _logoID) external nonReentrant {
         Logo memory l = logos[_logoID];
         l.isCrowdfunding = !l.isCrowdfunding;
         logos[_logoID] = l;
 
         emit CrowdfundToggle(msg.sender, l.isCrowdfunding);
-
-        return true;
     }
 
     function crowdfund(uint256 _logoID) external payable nonReentrant {
@@ -206,7 +141,7 @@ contract dLogos is ReentrancyGuard {
     }
 
     /**
-     * @dev Withdraw your pledge from a logo.
+     * @dev Withdraw your pledge from a Logo.
      */
     function withdrawFunds(
         uint256 _logoID,
@@ -238,7 +173,39 @@ contract dLogos is ReentrancyGuard {
     }
 
     /**
-     * @dev Return the list of backers for a logo.
+     * @dev Issue refund of the Logo.
+     */
+    function refund(
+        uint256 _logoID
+    ) external nonReentrant {
+        Logo memory l = logos[_logoID];
+        
+        bool c1 = l.creator == msg.sender; // Case 1: Logo admin can refund whenever.
+        bool c2 = block.timestamp > l.crowdfundEndAt; // Case 2: Crowdfund has not been completed by the due date.
+        bool c3 = l.scheduledAt != 0 && (block.timestamp > l.scheduledAt + 7 * 1 days) && !l.isUploaded; // Case 3: >7 days have passed since schedule date and no asset uploaded.
+        //TODO: Replace mediaAssetURL string length with isAccepted bool
+        //bool c4 = false; TODO: Case 3: >50% of backers reject upload.
+        require(c1 || c2 || c3, "No conditions met for refund.");
+        
+        Backer[] storage backers = logoBackers[_logoID];
+        
+        for (uint i = 0; i < backers.length; i++) {
+            require(
+                backers[i].isDistributed == false,
+                "Cannot refund backer funds that are already distributed."
+            );
+
+            uint256 amount = backers[i].amount;
+            address addr = backers[i].addr;
+            (bool success, ) = payable(addr).call{value: amount}("");
+            require(success, "Refund failed.");
+            delete backers[i];
+            emit RefundIssued(_logoID, addr, amount, c1, c2, c3);
+        }
+    }
+
+    /**
+     * @dev Return the list of backers for a Logo.
      */
     function getBackersForLogo(
         uint256 _logoID
@@ -247,14 +214,7 @@ contract dLogos is ReentrancyGuard {
     }
 
     /**
-     * @dev Return the list of convos for a creator.
-     */
-    // function getConvo(address _creator) external view returns(Convo[] memory) {
-    //     return conversations[_creator][];
-    // }
-
-    /**
-     * @dev Set speakers for a logo.
+     * @dev Set speakers for a Logo.
      */
     function setSpeakers(
         uint256 _logoID,
@@ -262,7 +222,7 @@ contract dLogos is ReentrancyGuard {
         uint16[] calldata _fees,
         string[] calldata _providers,
         string[] calldata _handles
-    ) external {
+    ) external nonReentrant {
         Logo memory l = logos[_logoID];
         require(l.creator == msg.sender); // Require msg sender to be the creator
         require(_speakers.length == _fees.length); // Equal speakers and fees
@@ -285,23 +245,21 @@ contract dLogos is ReentrancyGuard {
     /**
      * @dev Set status of a speaker.
      */
-    function setSpeakerStatus(uint256 _logoID, uint _status) external returns (bool) {
+    function setSpeakerStatus(uint256 _logoID, uint _status) external nonReentrant {
         
         Speaker[] memory speakers = logoSpeakers[_logoID];
         
         for (uint i = 0; i < speakers.length; i++) {
             if (address(speakers[i].addr) == msg.sender) { 
                 logoSpeakers[_logoID][i].status = SpeakerStatus(_status);
-                emit SpeakerStatusSet(msg.sender, _status);
+                emit SpeakerStatusSet(_logoID, msg.sender, _status);
                 break;
             }
         }
-
-        return true;
     }
 
     /**
-     * @dev Return the list of speakers for a logo.
+     * @dev Return the list of speakers for a Logo.
      */
     function getSpeakersForLogo(
         uint256 _logoID
@@ -312,9 +270,10 @@ contract dLogos is ReentrancyGuard {
     /**
      * @dev Set date for a conversation.
      */
-    function setDate(uint256 _logoID, uint _scheduledAt) external {
+    function setDate(uint256 _logoID, uint _scheduledAt) external nonReentrant {
         Logo memory l = logos[_logoID];
-
+        // TODO: prevent admin from continually pushing back date.
+        // TODO: make sure this is a future date
         require(l.creator == msg.sender); // Require msg sender to be the creator
 
         l.scheduledAt = _scheduledAt;
@@ -324,17 +283,18 @@ contract dLogos is ReentrancyGuard {
     }
 
     /*
-     * @dev Sets media URL for a logo.
+     * @dev Sets media URL for a Logo.
      */
     function setMediaAsset(
         uint256 _logoID,
         string calldata _mediaAssetURL
-    ) external {
+    ) external nonReentrant {
         Logo memory l = logos[_logoID];
 
         require(l.creator == msg.sender); // Require msg sender to be the creator
 
         l.mediaAssetURL = _mediaAssetURL;
+        l.isUploaded = true;
         logos[_logoID] = l; // add check prior to this
 
         emit MediaAssetSet(msg.sender, _mediaAssetURL);
---
+++ contracts/interfaces/IdLogos.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface IdLogos {
+
+    struct Backer {
+        address addr;
+        uint256 amount; // ETH Contribution
+        bool isDistributed; // Funds have been distributed
+    }
+
+    enum SpeakerStatus {
+        Pending,
+        Accepted,
+        Declined
+    }
+
+    struct Speaker {
+        address addr;
+        uint16 fee; // Speaker reward BPS
+        string provider; // e.g. X, Discord etc.
+        string handle;
+        SpeakerStatus status; // Status of the speaker 0 = pending, 1 = accepted, 2 = declined
+    }
+
+    struct Host {
+        address addr;
+        uint16 fee; // Host reward BPS
+    }
+
+    /// @notice All onchain information for a Logo.
+    struct Logo {
+        // Meta
+        uint256 id;
+        string title;
+        string description;
+        string discussion;
+        // Roles
+        address creator;
+        uint scheduledAt;
+        // Crowdfunding Attributes
+        uint crowdfundStartAt;
+        uint crowdfundEndAt;
+        bool isCrowdfunding;
+        /*
+        address[] speakers;
+        address[] backers;      // BackerInfo[] backers;
+    
+        
+
+        // Scheduling Attributes
+        uint scheduledAt;
+        uint scheduleFailedRefundAt; // Date to allow refund if conversation is not scheduled.
+        bool isScheduled;
+        
+        // Media Upload Attributes
+        uint uploadFailureRefundAt; // Date to allow refund if conversation is not uploaded.
+        
+        */
+        string mediaAssetURL;
+        bool isUploaded;
+        // Logo Split Address
+        address splits;
+    }
+
+}
